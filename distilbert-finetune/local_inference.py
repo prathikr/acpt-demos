5165ee1c9729a3dc1f8fcdfcb41242ce4042ba82
Prathik Rao <prathikrao@microsoft.com>
2022-10-12 16:00:56 -0700

    Testing

--- distilbert-finetune/local_inference.py
+++ distilbert-finetune/local_inference.py
@@ -35,17 +35,29 @@ def infer(args):
     model.load_state_dict(torch.load("pytorch_model.bin"))
     model.eval()
 
+    from datasets import Dataset
+    import pandas as pd
+    data = {
+        "id": [1, 2],
+        "title": ["University_of_Notre_Dame", "Songs"],
+        "context": ["Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend 'Venite Ad Me Omnes'. Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary.", "The best song ever is Candy Paint by Post Malone."],
+        "question": ["To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France?", "What is the best song ever?"]
+    }
+    df = pd.DataFrame(data)
+    df_dataset = Dataset.from_pandas(df)
+
+    test_data = {"test": df_dataset}
+    test_df = pd.DataFrame(test_data)
+    test_df_dataset = Dataset.from_pandas(test_df)
+    print("test_df_dataset", test_df_dataset)
 
     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-    from datasets import load_dataset
-    squad = load_dataset("squad")
-    print(squad.head())
-    tokenized_squad = squad.map(preprocess_function, fn_kwargs={"tokenizer": tokenizer}, batched=True, remove_columns=squad["train"].column_names)
-    
+    tokenized_inputs = test_df_dataset.map(preprocess_function, fn_kwargs={"tokenizer": tokenizer}, batched=True, remove_columns=test_df_dataset["test"].column_names)
+
     if args.run_config == "no_acc":
         model.to(device)
-        inputs.to(device)
-        predictions = model(inputs)
+        tokenized_inputs = tokenized_inputs.to(device)
+        predictions = model(tokenized_inputs)
     elif args.run_config == "ort":
         import onnxruntime
 
