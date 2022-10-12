7fdb340e1d33b89deb8f3e418664a44f6314a204
Prathik Rao <prathikrao@microsoft.com>
2022-10-12 15:50:10 -0700

    Testing

--- distilbert-finetune/local_inference.py
+++ distilbert-finetune/local_inference.py
@@ -35,26 +35,26 @@ def infer(args):
     model.load_state_dict(torch.load("pytorch_model.bin"))
     model.eval()
 
-    test_inputs = [{
-        "context": "Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend 'Venite Ad Me Omnes'. Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary.",
-        "question": "To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France?"
-    }]
-    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-    inputs = preprocess_function(test_inputs, tokenizer)
 
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    from datasets import load_dataset
+    squad = load_dataset("squad")
+    print(squad.head())
+    tokenized_squad = squad.map(preprocess_function, fn_kwargs={"tokenizer": tokenizer}, batched=True, remove_columns=squad["train"].column_names)
+    
     if args.run_config == "no_acc":
         model.to(device)
         inputs.to(device)
-        outputs = model(inputs)
+        predictions = model(inputs)
     elif args.run_config == "ort":
         import onnxruntime
 
         torch.export.onnx(model, "model.onnx")
         ort_session = onnxruntime.InferenceSession('model.onnx')
-        outputs = ort_session.run(None, {'input': inputs})        
+        predictions = ort_session.run(None, {'input': inputs})        
 
     print(f'Input: "{inputs}"')
-    print(f'Prediction: "{outputs}"')
+    print(f'Prediction: "{predictions}"')
 
 def main(raw_args=None):
     args = get_args(raw_args)
