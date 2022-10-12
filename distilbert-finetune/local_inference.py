ec6f47e550e8516b01091de1dcf1b416f59578a9
Prathik Rao <prathikrao@microsoft.com>
2022-10-12 13:22:02 -0700

    load model properly

--- distilbert-finetune/local_inference.py
+++ distilbert-finetune/local_inference.py
@@ -2,6 +2,7 @@ import argparse
 import json
 
 import torch
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -18,7 +19,10 @@ def get_args(raw_args=None):
     return args
 
 def infer(args):
-    model = torch.load("pytorch_model.bin")
+    model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-uncased")
+    model.load_state_dict(torch.load("pytorch_model.bin"))
+    model.eval()
+
     test_input = {
         "context": "Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend 'Venite Ad Me Omnes'. Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary.",
         "question": "To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France?"
