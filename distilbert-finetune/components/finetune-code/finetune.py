1e3aa7f5070b5fd30829bbdc060c0c379b30b1a3
Prathik Rao <prathikrao@microsoft.com>
2022-10-12 11:43:44 -0700

    upgrade to 50 epochs to test performance improvements

--- distilbert-finetune/components/finetune-code/finetune.py
+++ distilbert-finetune/components/finetune-code/finetune.py
@@ -92,7 +92,7 @@ def main(
         "per_device_train_batch_size": 16,
         "per_device_eval_batch_size": 16,
         "learning_rate": 2e-5,
-        "num_train_epochs": 10,
+        "num_train_epochs": 50,
         "weight_decay": 0.01,
         "fp16": True,
         "deepspeed": "ds_config_zero_1.json" if deepspeed else None,
