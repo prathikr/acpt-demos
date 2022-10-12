30f58db9ca5bfc5f365469731ccfd5e8fae8e306
Prathik Rao <prathikrao@microsoft.com>
2022-10-12 16:21:13 -0700

    Testing

--- distilbert-finetune/local_inference.py
+++ distilbert-finetune/local_inference.py
@@ -51,7 +51,14 @@ def infer(args):
     # print("test_df_datasetdict\n", test_df_dataset)
 
     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-    inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+    inputs = tokenizer(
+        ["To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France?", "What is the best song ever?"],
+        ["Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend 'Venite Ad Me Omnes'. Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary.", "The best song ever is Candy Paint by Post Malone."],
+        max_length=384,
+        truncation="only_second",
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
 
     if args.run_config == "no_acc":
         predictions = model(**inputs)
