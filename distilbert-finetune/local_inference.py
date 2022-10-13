82d4208044ee8e0d3fabc8f82167541c3d12ab68
Prathik Rao <prathikrao@microsoft.com>
2022-10-12 18:06:44 -0700

    testing

--- distilbert-finetune/local_inference.py
+++ distilbert-finetune/local_inference.py
@@ -29,17 +29,31 @@ def infer(args):
     context = "Beyonce Giselle Knowles-Carter (born September 4, 1981) is an American singer, songwriter, record producer and actress. Born and raised in Houston, Texas, she performed in various singing and dancing competitions as a child, and rose to fame in the late 1990s as lead singer of R&B girl-group Destiny's Child. Managed by her father, Mathew Knowles, the group became one of the world's best-selling girl groups of all time. Their hiatus saw the release of Beyoncé's debut album, Dangerously in Love (2003), which established her as a solo artist worldwide, earned five Grammy Awards and featured the Billboard Hot 100 number-one singles 'Crazy in Love' and 'Baby Boy'."
     questions = ["When was Beyonce born?", "What areas did Beyonce compete in when she was growing up?", "When did Beyonce leave Destiny's Child and become a solo singer?", "What was the name of Beyonce's debut album?"]
     
-    tokenizer_inputs = []
-    for question in questions:
-        tokenizer_inputs.append([question, context])
+    inputs = []
+    for question in questions :
+        inputs.append((question, context))
 
-    inputs = tokenizer(tokenizer_inputs, return_tensors="pt", padding=True)
-    start = time.time()
-    outputs = model(**inputs)
-    end = time.time()
+    encoding = tokenizer.batch_encode_plus(inputs, pad_to_max_length=True, return_tensors="pt")
+    input_ids, attention_mask = encoding["input_ids"], encoding["attention_mask"]
+    start_scores, end_scores = model(input_ids, attention_mask=attention_mask)
 
-    tmp = tokenizer.decode(outputs)
-    print(tmp)
+    for index,(start_score, end_score, input_id) in enumerate(zip(start_scores, end_scores, input_ids)):
+        max_startscore = torch.argmax(start_score)
+        max_endscore = torch.argmax(end_score)
+        ans_tokens = input_ids[index][max_startscore: max_endscore + 1]
+        answer_tokens = tokenizer.convert_ids_to_tokens(ans_tokens, skip_special_tokens=True)
+        answer_tokens_to_string = tokenizer.convert_tokens_to_string(answer_tokens)
+        print("Question: ", questions[index])
+        print("Answer: ", answer_tokens_to_string)
+
+    # tokenizer_inputs = []
+    # for question in questions:
+    #     tokenizer_inputs.append([question, context])
+
+    # inputs = tokenizer(tokenizer_inputs, return_tensors="pt", padding=True)
+    # start = time.time()
+    # outputs = model(**inputs)
+    # end = time.time()
 
     # for i in range(len(questions)):
     #     answer_start_index = outputs.start_logits[i].argmax()
