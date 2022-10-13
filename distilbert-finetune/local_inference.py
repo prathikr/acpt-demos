ecfa7519287557223c76344823fe00394349a2a7
Prathik Rao <prathikrao@microsoft.com>
2022-10-12 17:43:24 -0700

    fixed bug

--- distilbert-finetune/local_inference.py
+++ distilbert-finetune/local_inference.py
@@ -1,8 +1,9 @@
 import argparse
-import json
+import time
 
 import torch
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer
+import onnxruntime
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -27,17 +28,24 @@ def infer(args):
 
     context = "Beyonce Giselle Knowles-Carter (born September 4, 1981) is an American singer, songwriter, record producer and actress. Born and raised in Houston, Texas, she performed in various singing and dancing competitions as a child, and rose to fame in the late 1990s as lead singer of R&B girl-group Destiny's Child. Managed by her father, Mathew Knowles, the group became one of the world's best-selling girl groups of all time. Their hiatus saw the release of Beyoncé's debut album, Dangerously in Love (2003), which established her as a solo artist worldwide, earned five Grammy Awards and featured the Billboard Hot 100 number-one singles 'Crazy in Love' and 'Baby Boy'."
     questions = ["When was Beyonce born?", "What areas did Beyonce compete in when she was growing up?", "When did Beyonce leave Destiny's Child and become a solo singer?", "What was the name of Beyonce's debut album?"]
+    
+    onnx_input = [tokenizer(questions[0], context, return_tensors="pt")]
+    torch.onnx.export(model, onnx_input, "onnx_model.onnx", export_params=True)
+    ort_session = onnxruntime.InferenceSession("onnx_model.onnx")
+    
     print("Context: ", context)
+    total_inferencing_time = 0
     for question in questions:
         inputs = tokenizer(question, context, return_tensors="pt")
 
+        start = time.time()
         if args.run_config == "no_acc":
             outputs = model(**inputs)
         elif args.run_config == "ort":
-            import onnxruntime
-            torch.onnx.export(model, **inputs,"onnx_model.onnx", export_params=True)
-            ort_session = onnxruntime.InferenceSession("onnx_model.onnx")
             outputs = ort_session.run(None, {'input': inputs})[0]
+        end = time.time()
+
+        total_inferencing_time += end - start
 
         answer_start_index = outputs.start_logits.argmax()
         answer_end_index = outputs.end_logits.argmax()
@@ -46,6 +54,8 @@ def infer(args):
         prediction = tokenizer.decode(predict_answer_tokens)
         print("Question: ", question)
         print("Answer: ", prediction)
+
+    print("Total inferencing time: ", total_inferencing_time)
 
 def main(raw_args=None):
     args = get_args(raw_args)
