c22b69be81a6f17b0423c01a06a9668d3fae8b39
Prathik Rao <prathikrao@microsoft.com>
2022-10-12 17:04:04 -0700

    works!

--- distilbert-finetune/local_inference.py
+++ distilbert-finetune/local_inference.py
@@ -36,11 +36,12 @@ def infer(args):
 
     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
+    context = "Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend 'Venite Ad Me Omnes'. Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary."
+
     question1 = "To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France?"
-    context1 = "Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend 'Venite Ad Me Omnes'. Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary."
-    
-    question2 = "What is the best song ever?"
-    context2 = "The best song ever is Candy Paint by Post Malone."
+    question2 = "What is in front of the Notre Dame Main Building?"
+    question3 = "The Basilica of the Sacred heart at Notre Dame is beside to which structure?"
+    question4 = "What is the Grotto at Notre Dame?"
 
     data = [(question1, context1), (question2, context2)]
 
