c344b488b37f233c0f572934d89bd4a0c92c71ad
Prathik Rao <prathikrao@microsoft.com>
2022-10-12 17:25:25 -0700

    queen

--- distilbert-finetune/local_inference.py
+++ distilbert-finetune/local_inference.py
@@ -24,8 +24,8 @@ def infer(args):
 
     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
-    context = "Beyoncé Giselle Knowles-Carter (born September 4, 1981) is an American singer, songwriter, record producer and actress. Born and raised in Houston, Texas, she performed in various singing and dancing competitions as a child, and rose to fame in the late 1990s as lead singer of R&B girl-group Destiny's Child. Managed by her father, Mathew Knowles, the group became one of the world's best-selling girl groups of all time. Their hiatus saw the release of Beyoncé's debut album, Dangerously in Love (2003), which established her as a solo artist worldwide, earned five Grammy Awards and featured the Billboard Hot 100 number-one singles 'Crazy in Love' and 'Baby Boy'."
-    questions = ["When did Beyonce start becoming popular?", "What areas did Beyonce compete in when she was growing up?", "When did Beyonce leave Destiny's Child and become a solo singer?", "In what city and state did Beyonce grow up?"]
+    context = "Beyonce Giselle Knowles-Carter (born September 4, 1981) is an American singer, songwriter, record producer and actress. Born and raised in Houston, Texas, she performed in various singing and dancing competitions as a child, and rose to fame in the late 1990s as lead singer of R&B girl-group Destiny's Child. Managed by her father, Mathew Knowles, the group became one of the world's best-selling girl groups of all time. Their hiatus saw the release of Beyoncé's debut album, Dangerously in Love (2003), which established her as a solo artist worldwide, earned five Grammy Awards and featured the Billboard Hot 100 number-one singles 'Crazy in Love' and 'Baby Boy'."
+    questions = ["When was Beyonce born?", "What areas did Beyonce compete in when she was growing up?", "When did Beyonce leave Destiny's Child and become a solo singer?", "Which popular girl-group was Beyonce the lead singer of?", "Which Beyonce songs were featured on the Billboard Hot 100 number-one singles list?"]
     for question in questions:
         inputs = tokenizer(question, context, return_tensors="pt")
         with torch.no_grad():
