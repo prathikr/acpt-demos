d761ba8e03c48328fc9341f2e30832dfe34390fa
Prathik Rao <prathikrao@microsoft.com>
2022-10-19 15:35:30 -0700

    clean code

--- distilbert-finetune/inference.py
+++ distilbert-finetune/inference.py
@@ -19,14 +19,13 @@ def infer(args):
 
     # load test data
     context = """
-              Beyonce Giselle Knowles-Carter (born September 4, 1981) is an American singer, songwriter, record producer and actress.
-              Born and raised in Houston, Texas, she performed in various singing and dancing competitions as a child, and rose to fame
-              in the late 1990s as lead singer of R&B girl-group Destiny's Child. Managed by her father, Mathew Knowles, the group became
-              one of the world's best-selling girl groups of all time. Their hiatus saw the release of Beyoncé's debut album, Dangerously
-              in Love (2003), which established her as a solo artist worldwide, earned five Grammy Awards and featured the Billboard Hot
-              100 number-one singles 'Crazy in Love' and 'Baby Boy'.
-
-              """
+        Beyonce Giselle Knowles-Carter (born September 4, 1981) is an American singer, songwriter, record producer and actress.
+        Born and raised in Houston, Texas, she performed in various singing and dancing competitions as a child, and rose to fame
+        in the late 1990s as lead singer of R&B girl-group Destiny's Child. Managed by her father, Mathew Knowles, the group became
+        one of the world's best-selling girl groups of all time. Their hiatus saw the release of Beyoncé's debut album, Dangerously
+        in Love (2003), which established her as a solo artist worldwide, earned five Grammy Awards and featured the Billboard Hot
+        100 number-one singles 'Crazy in Love' and 'Baby Boy'.
+        """
 
     questions = ["When was Beyonce born?", 
                  "What areas did Beyonce compete in when she was growing up?", 
